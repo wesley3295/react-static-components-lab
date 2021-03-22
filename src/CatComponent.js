80b1a327fe09a65e241ab607d3ababe02f7bf1ff
Wesley <wspencer3295@yahoo.com>
2021-03-22 13:28:04 -0700

    commit tree 859a273c9187f15a62b17b0b4bd220319c5656cd

--- src/CatComponent.js
+++ src/CatComponent.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-
-export default class CatComponent extends Component {
+ class CatComponent extends Component {
   render() {
     return (
       <div className="bar" id="cat">
@@ -9,3 +8,4 @@ export default class CatComponent extends Component {
     );
   }
 }
+export default CatComponent
